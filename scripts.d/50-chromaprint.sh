1ad056a7f200477011c422243eac9f92ea5080d0
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2025-03-28 02:36:18 +0100

    t

---
+++ scripts.d/50-chromaprint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CHROMAPRINT_REPO="https://github.com/acoustid/chromaprint.git"
+CHROMAPRINT_COMMIT="master"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$CHROMAPRINT_REPO" "$CHROMAPRINT_COMMIT" chromaprint
+    cd chromaprint
+
+    mkdir build && cd build
+
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+        -DBUILD_{SHARED_LIBS,TESTS,TOOLS}=OFF \
+        -DFFT_LIB=fftw3 \
+        -GNinja \
+        ..
+    ninja -j"$(nproc)"
+    ninja install
+
+    {
+        echo "Libs.private: -lfftw3 -lstdc++"
+        echo "Cflags.private: -DCHROMAPRINT_NODLL"
+    } >> "$FFBUILD_PREFIX"/lib/pkgconfig/libchromaprint.pc
+}
+
+ffbuild_configure() {
+    echo --enable-chromaprint
+}
+
+ffbuild_unconfigure() {
+    echo --disable-chromaprint
+}
