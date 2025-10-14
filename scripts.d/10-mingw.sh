a2c161b86bd04f7d34ee84af050c1af388ad7182
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2025-10-14 18:54:56 +0200

    fork

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#MINGW_REPO="https://git.code.sf.net/p/mingw-w64/mingw-w64.git"
 MINGW_REPO="https://github.com/mingw-w64/mingw-w64.git"
-MINGW_COMMIT="63015ba82b6bee84cf7540e4fdc1fe2e54401e6f"
+MINGW_COMMIT="f6a7b25bc70e9c3f5d0a8535ad112b08de564b33"
 
 ffbuild_enabled() {
     [[ $TARGET == win* ]] || return -1
