d8c2888c06e4c5b9445a1f684e5047e721d6fc17
Kosm8 <77066077+Kosm8@users.noreply.github.com>
2025-10-14 18:54:56 +0200

    fork

--- scripts.d/10-mingw.sh
+++ scripts.d/10-mingw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#MINGW_REPO="https://git.code.sf.net/p/mingw-w64/mingw-w64.git"
 MINGW_REPO="https://github.com/mingw-w64/mingw-w64.git"
 MINGW_COMMIT="ddda991ad0f6f0531a6e2058f3f187a552201097"
 
