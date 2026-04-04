b01a6f1c23311744a40c4cc8318415c2cfbcfa8c
nanake <nanake@users.noreply.github.com>
2026-04-04 19:42:54 +0900

    Add spirv-headers for vulkan
    https://code.ffmpeg.org/FFmpeg/FFmpeg/pulls/22609/commits
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

---
+++ scripts.d/50-vulkan/35-spirv-headers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SPIRVHEADERS_REPO="https://github.com/KhronosGroup/SPIRV-Headers.git"
+SPIRVHEADERS_COMMIT="6dd7ba990830f7c15ac1345ff3b43ef6ffdad216"
+
+ffbuild_enabled() {
+    return 0
+}
+
+ffbuild_dockerbuild() {
+    git-mini-clone "$SPIRVHEADERS_REPO" "$SPIRVHEADERS_COMMIT" spirv-headers
+    cd spirv-headers
+
+    mkdir build && cd build
+
+    cmake \
+        -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
+        -DSPIRV_HEADERS_ENABLE_INSTALL=ON \
+        -DSPIRV_HEADERS_ENABLE_TESTS=OFF \
+        -GNinja \
+        ..
+    ninja -j"$(nproc)"
+    ninja install
+}
