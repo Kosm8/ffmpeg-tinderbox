9104b3f2e05d4b979e73257bf49c985bf4841220
nanake <nanake@users.noreply.github.com>
2026-04-14 22:40:21 +0900

    Upgrade OpenSSL to 4.0.0
    https://github.com/openssl/openssl/releases/tag/openssl-4.0.0
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-protocol/25-openssl.sh
+++ scripts.d/50-protocol/25-openssl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 OPENSSL_REPO="https://github.com/openssl/openssl.git"
-OPENSSL_COMMIT="openssl-3.6.2"
-OPENSSL_TAGFILTER="openssl-3.6.*"
+OPENSSL_COMMIT="openssl-4.0.0"
+OPENSSL_TAGFILTER="openssl-4.0.*"
 
 ffbuild_enabled() {
     return 0
