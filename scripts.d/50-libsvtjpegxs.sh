c127c637ca6c64ba0e256b5779d1ea9334417584
nanake <nanake@users.noreply.github.com>
2026-08-14 08:56:03 +0900

    libsvtjpegxs: don't build testing
    fixes build bustage regarding link error
    
    Signed-off-by: nanake <nanake@users.noreply.github.com>

--- scripts.d/50-libsvtjpegxs.sh
+++ scripts.d/50-libsvtjpegxs.sh
@@ -17,7 +17,7 @@ ffbuild_dockerbuild() {
         -DCMAKE_TOOLCHAIN_FILE="$FFBUILD_CMAKE_TOOLCHAIN" \
         -DCMAKE_BUILD_TYPE=Release \
         -DCMAKE_INSTALL_PREFIX="$FFBUILD_PREFIX" \
-        -DBUILD_{APPS,SHARED_LIBS}=OFF \
+        -DBUILD_{APPS,SHARED_LIBS,TESTING}=OFF \
         -DCOVERAGE=OFF \
         -GNinja \
         ..
